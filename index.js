76ad4b40c62df6f189eff23313b0f631e8bf0d48
riverita98 <riverarmd98@gmail.com>
2021-02-05 18:44:24 +0100

    bot

--- index.js
+++ index.js
@@ -4,47 +4,76 @@ var bodyParser = require('body-parser');
 const Discord = require("discord.js");
 const config = require("./config.json");
 
-const client = new Discord.Client();
+//const client = new Client({ ws: { intents: ['GUILDS', 'GUILD_MEMBERS'] } });
+const client = new Discord.Client({ intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS'] });
 const prefix = "/";
 
-//Si el mensaje proviene de un bot se obvia
+//inicio de sesion
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.on("message", function (message) {
+
+
+client.on("message", async function (message) {
+    //Si el mensaje proviene de un bot se obvia
+
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
     // Se separa el comando y argumentos divididos por espacio
     const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  }
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply();
+    }
+
+    if (command === "event") {
+
+        console.log("Enviando MD sobre evento");
+
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(toString(args));
+
+        message.reply("ENVIANDO NOTIFICACIONES SOBRE EVENTO");
+        message.guild.members.fetch()
+            .then(e => {
+                console.log(e);
+                const memberList = e.toJSON();
+                memberList.forEach(member => {
+                    client.users.fetch(member['userID']).then(user => {
+
+                        if (member['displayName'] != "GypsyBot" && member['displayName'] != "BoobBot™"
+                            && member['displayName'] != "Groovy" && member['displayName'] != "Casino") {
+                            console.log(member['displayName'] + "-" + member['userID']);
+
+                            user.send(toString(args));
+                        }
+
+                    })
+                });
+                message.reply("NOTIFICACIONES DE EVENTO ENVIADAS CORRECTAMENTE");
+            })
+            .catch(console.error);
+    }
 
 });
 
 client.login(config.BOT_TOKEN);
 
-/*
-var app = express();
-var port = process.env.PORT || 3525;
 
-// Convierte una petición recibida (POST-GET...) a objeto JSON
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-    res.status(200).send({
-        message: 'GET Home route working fine!'
+function toString(arr) {
+    result = "";
+    arr.forEach(element => {
+        result += " " + element;
     });
-});
+    return result;
+}
 
-app.listen(port, function(){
-    console.log(`Server running in http://localhost:${port}`);
-    console.log('Defined routes:');
-    console.log('	[GET] http://localhost:3525/');
-});
 
-*/
+
+
